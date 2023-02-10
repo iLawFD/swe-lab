a5da5844a893c0527f5d3e58a9b086d32b3ba765
Fahad <ggforever111@gmail.com>
2023-02-10 11:21:30 +0300

    changed

---
+++ Main.java
@@ -0,0 +1,12 @@
+package swelab;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellwo!");
+
+    }
+
+    public int sortM(){
+        return 1;
+    }
+
+}
