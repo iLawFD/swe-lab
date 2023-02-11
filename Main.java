1b05dc290541f221359f3c8f6d0533bb6d3b8661
Fahad <ggforever111@gmail.com>
2023-02-11 20:10:34 +0300

    added a selction method

--- Main.java
+++ Main.java
@@ -5,9 +5,34 @@ public class Main {
     }
 
 
-    public static void m1(){
+    void sort(int arr[])
+    {
+        int n = arr.length;
 
-        return;
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Prints the array
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
     }
 }
 
