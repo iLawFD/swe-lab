11feed56887464572724ebeef9235b456be287ed
Omar Alawaji <s202025780@kfupm.edu.sa>
2023-02-13 04:11:31 -0800

    modifed the main to call the sort() method

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         int [] arr = {5,2,12,12,1};
-        sortInsertion(arr);
+        sort(arr);
 
         for(int i=0;i<arr.length;++i){
           System.out.print(arr[i] + " ");
@@ -11,19 +11,7 @@ public class Main {
 
     public static void sort(int [] sort_arr){
 
-      for(int i=0;i<sort_arr.length;++i){
-
-        int j = i;
-        
-        while(j > 0 && sort_arr[j-1]>sort_arr[j]){
-
-          int key = sort_arr[j];
-          sort_arr[j] = sort_arr[j-1];
-          sort_arr[j-1] = key;
-          j = j-1; 
-
-        }
-      }
+      sortInsertion(sort_arr);
     }
     void Selectionsort(int arr[])
     {
