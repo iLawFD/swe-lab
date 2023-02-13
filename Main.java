1b231d52c946f4e6f5ab658aae9914777155839a
Omar Alawaji <s202025780@kfupm.edu.sa>
2023-02-13 04:08:46 -0800

    added sort() method that uses insertion sort

--- Main.java
+++ Main.java
@@ -9,7 +9,22 @@ public class Main {
         }
     }
 
+    public static void sort(int [] sort_arr){
 
+      for(int i=0;i<sort_arr.length;++i){
+
+        int j = i;
+        
+        while(j > 0 && sort_arr[j-1]>sort_arr[j]){
+
+          int key = sort_arr[j];
+          sort_arr[j] = sort_arr[j-1];
+          sort_arr[j-1] = key;
+          j = j-1; 
+
+        }
+      }
+    }
     void Selectionsort(int arr[])
     {
         int n = arr.length;
